ef7b2607d806b55b7af823f01b3c2ec4f4bafe47
Roger <rogeliomita@gmail.com>
2015-09-10 20:29:46 -0300

    Modelo de datos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+struct Categoria {
+  unsigned int codigo;
+  string descripcion;
+};
+
+struct Prestatario {
+  unsigned int codigo;
+  string apellido;
+  string nombre;
+};
+
+struct Prestamo {
+  Categoria *categoria;
+  Prestatario *prestatario;
+  string descripcion;
+  bool estado;
+};
+
 int main()
 {
+    Categoria libros = {1, "libros"};
+    Prestatario carlos = {1, "Chiariotti", "Carlos"};
+    Prestamo prestamo = {&libros, &carlos, "Agilmente", true};
+
+    cout << (*prestamo.categoria).descripcion << " - " << (*prestamo.prestatario).apellido;
+
     return 0;
 }
 
