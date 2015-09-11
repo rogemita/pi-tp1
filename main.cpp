a052322d24b6ea50fa63b1e2d3e7af0a18b528e6
Roger <rogeliomita@gmail.com>
2015-09-10 21:29:47 -0300

    Menues de nivel 2

--- main.cpp
+++ main.cpp
@@ -23,7 +23,6 @@ struct Prestamo {
 };
 
 struct Menu {
-  int nivel;
   int cant_opciones;
   string opciones[7];
 };
@@ -37,15 +36,44 @@ void dibujar_menu(Menu *menu) {
 
 int main() {
   int opcion = -1;
-  Menu nivel_1 = {1, 3, {
+  Menu nivel_1 = {3, {
     "Administrar y consultar Categorías y Prestatarios",
     "Administrar Préstamos",
     "Consultar Préstamos"}
   };
+  Menu nivel_1_1 = {6, {
+    "Agregar categoría",
+    "Modificar categoría",
+    "Eliminar categoría",
+    "Agregar prestatario",
+    "Modificar prestatario",
+    "Eliminar prestatario"}
+  };
+  Menu nivel_1_2 = {4, {
+    "Agregar préstamo",
+    "Modificar préstamo",
+    "Eliminar préstamo",
+    "Devolver préstamo"}
+  };
+  Menu nivel_1_3 = {4, {
+    "Cantidad de objetos prestados por categoría",
+    "Listado de préstamos por categoría",
+    "Listado de préstamos ordenados por categoría o prestatario",
+    "Listar todos los prestatarios que tienen al menos un objeto prestado"}
+  };
   do {
     switch (opcion) {
-     default:
-        dibujar_menu(&nivel_1);
+      case 1:
+        dibujar_menu(&nivel_1_1);
+        break;
+      case 2:
+        dibujar_menu(&nivel_1_2);
+        break;
+      case 3:
+        dibujar_menu(&nivel_1_3);
+        break;
+       default:
+         dibujar_menu(&nivel_1);
     }
     cout << "ingrese opcion: ";
     cin >> opcion;
