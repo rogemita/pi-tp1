ac50d07c29e89d6ec10ad2b607692943a70d40fe
Roger <rogeliomita@gmail.com>
2015-09-10 21:19:41 -0300

    Menu de nivel 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iterator>
 
 using namespace std;
 
@@ -21,14 +22,35 @@ struct Prestamo {
   bool estado;
 };
 
-int main()
-{
-    Categoria libros = {1, "libros"};
-    Prestatario carlos = {1, "Chiariotti", "Carlos"};
-    Prestamo prestamo = {&libros, &carlos, "Agilmente", true};
+struct Menu {
+  int nivel;
+  int cant_opciones;
+  string opciones[7];
+};
 
-    cout << (*prestamo.categoria).descripcion << " - " << (*prestamo.prestatario).apellido;
+void dibujar_menu(Menu *menu) {
+    cout << "0 - Salir del programa" << endl;
+    for(int i=0; i < menu->cant_opciones; i++) {
+      cout << i+1 << " - " << menu->opciones[i] << endl;
+    }
+}
 
-    return 0;
+int main() {
+  int opcion = -1;
+  Menu nivel_1 = {1, 3, {
+    "Administrar y consultar Categorías y Prestatarios",
+    "Administrar Préstamos",
+    "Consultar Préstamos"}
+  };
+  do {
+    switch (opcion) {
+     default:
+        dibujar_menu(&nivel_1);
+    }
+    cout << "ingrese opcion: ";
+    cin >> opcion;
+  } while (opcion != 0);
+
+  return 0;
 }
 
